ed39648be2c4c46f3ca49684c36acf3ec16c9da4
Keyrixz <111528658+Keyrixz@users.noreply.github.com>
2025-02-07 22:10:23 +0300

    Update keyanticheatload.lua

--- keyanticheatload.lua
+++ keyanticheatload.lua
@@ -1,4 +1,6 @@
-while true do
-  wait(1)
-  warn("THIS GAME PROTECTING WITH KEY ANTI-CHEAT!")
-end
+task.spawn(function()
+    while true do
+        task.wait(1)  -- Daha performanslı bekleme
+        print("🔒 THIS GAME IS PROTECTED WITH ANTI-CHEAT SYSTEM!")  -- Daha iyi mesaj
+    end
+end)
